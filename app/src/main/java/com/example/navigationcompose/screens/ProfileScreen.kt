a12dc181ca44ba1b81975ffe049d449b4573fce1
Hanzilah Sheikh <hanzilah384@gmail.com>
2026-02-19 17:11:15 +0500

    v1.1

--- app/src/main/java/com/example/navigationcompose/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/navigationcompose/screens/ProfileScreen.kt
@@ -1,27 +1,77 @@
 package com.example.navigationcompose.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.navigationcompose.R
 
 @Composable
 fun ProfileScreen() {
 
     Column(
-        Modifier.fillMaxSize(),
+        Modifier
+            .fillMaxSize()
+            .background(Color.Gray),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Profile Screen", style = MaterialTheme.typography.displayMedium )
-        Text("Hello, This is a Profile Screen")
-    }
 
+        Image(
+            painter = painterResource(id = R.drawable.ic_login),
+            contentDescription = null,
+            Modifier.size(200.dp)
+        )
+
+        Text(
+            "Profile Screen",
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.padding(top = 10.dp)
+        )
+        Text("Hello, This is a Profile Screen")
+
+        Row(
+            Modifier.padding(top = 20.dp)
+        ) {
+
+            Button(
+                onClick = {}, colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black
+                ), modifier = Modifier.padding(horizontal = 10.dp)
+            ) {
+                Text("Login")
+            }
+
+            Button(
+                onClick = {}, colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black
+                ), modifier = Modifier.padding(horizontal = 10.dp)
+            ) {
+                Text("Sign up")
+            }
+        }
+
+        Text(
+            text = "Please Sign up or Login to the account to proceed!",
+            modifier = Modifier.padding(top = 10.dp),
+            style = MaterialTheme.typography.labelSmall,
+        )
+    }
 }
 
 @Preview
--- app/src/main/java/com/example/navigationcompose/screens/SearchScreen.kt
+++ app/src/main/java/com/example/navigationcompose/screens/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.example.navigationcompose.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,13 +9,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SearchScreen() {
 
     Column(
-        Modifier.fillMaxSize(),
+        Modifier.fillMaxSize()
+            .background(Color.Yellow),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
